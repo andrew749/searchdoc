adc563dd9bfdbebd18dd3dcf12b5d1932c52e51e
Andrew Codispoti <andrewcod749@gmail.com>
2017-10-20 14:29:26 -0400

    Added basic main method to process command line input.

---
+++ src/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var language string
+var queryType string
+
+func printUsage(programName string) {
+	fmt.Printf("./%s [--query QUERY] [--language] [--type]* \n", programName)
+	os.Exit(1)
+}
+
+/**
+ * Result from a query into the db storing language specific results.
+ */
+type QueryResult struct {
+	ResultName     string
+	ResultType     string
+	ResultLanguage string
+}
+
+// process a specific query based on the
+func processCommand(query string, language string, queryType string) {
+
+}
+
+func main() {
+
+	// get the arguments besdi
+
+	if len(os.Args) < 2 {
+		// not enough arguments
+		printUsage(os.Args[0])
+	}
+
+	query := os.Args[1]
+
+	var language string
+
+	if len(os.Args) >= 3 {
+		language = os.Args[2]
+	}
+
+	var queryType string
+
+	if len(os.Args) >= 4 {
+		queryType = os.Args[3]
+	}
+
+	// process the command
+	processCommand(query, language, queryType)
+
+}
