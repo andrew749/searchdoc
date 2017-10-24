769226d860c6a4af8c2cb89b77898c54448a8a33
Andrew Codispoti <andrewcod749@gmail.com>
2017-10-24 17:41:06 -0400

    Preference manager automatically created necessary files.

--- src/preference_manager.go
+++ src/preference_manager.go
@@ -5,18 +5,60 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-const PreferencesPath = "~/.searchdoc/preferences.json"
+func PreferencesDirectory() string {
+	path := filepath.Join(os.Getenv("HOME"), ".searchdoc")
+
+	return path
+}
+
+const PreferencesFile = "preferences.json"
+
+func PreferencesPath() string {
+	path := filepath.Join(PreferencesDirectory(), PreferencesFile)
+
+	return path
+}
 
 type Preferences struct {
-	DatabasePath string `json:"database_path"`
+	DocsetPath string `json:"docset_path"`
 }
 
 func loadPreferences() Preferences {
 	var data Preferences
 
-	file, err := os.Open(PreferencesPath)
+	var file *os.File
+	if _, err := os.Stat(PreferencesPath()); os.IsNotExist(err) {
+
+		if _, err = os.Stat(PreferencesDirectory()); os.IsNotExist(err) {
+			log.Println("Trying to create directory")
+
+			err2 := os.Mkdir(PreferencesDirectory(), 0777)
+
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+
+		}
+
+		log.Println("Trying to create file")
+		file, err2 := os.Create(PreferencesPath())
+
+		if err2 != nil {
+			log.Println("Cant create")
+			log.Fatal(err2)
+		}
+
+		file.WriteString("{}")
+
+		log.Println("Created file")
+	}
+
+	log.Println("DONE")
+
+	file, err := os.Open(PreferencesPath())
 
 	if err != nil {
 		log.Fatal(err)
@@ -41,6 +83,6 @@ func loadPreferences() Preferences {
 
 var _Preferences = loadPreferences()
 
-func getPreferences() Preferences {
+func GetPreferences() Preferences {
 	return _Preferences
 }
